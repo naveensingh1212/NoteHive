367afdc7123f58d8a44be48750168e9cedbe3eb0
naveensingh1212 <nnaveensingh0101@gmail.com>
2025-04-29 22:07:59 +0530

    Refactor user routes for improved readability and structure

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,10 +1,16 @@
-
 import express from "express";
-import { registerUser,loginUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
+import { authenticator } from  "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/register",registerUser);
-router.post("/login",loginUser);
+// Public Routes
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+//protected routes
+router.get("/profile", authenticator, (req, res) => {
+  res.json({ message: `Welcome ${req.user.email}` });
+});
 
 export default router;
