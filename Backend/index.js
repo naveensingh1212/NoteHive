6986da94e7620d75afa8c082e74f4f9a2cedc4b7
naveensingh1212 <nnaveensingh0101@gmail.com>
2025-05-04 11:25:21 +0530

    refactor: simplify port handling in server initialization

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,12 @@ import app from './app.js';
 dotenv.config();
 
 const DB_NAME = process.env.DB_NAME;
+const PORT = process.env.PORT || 8000; // Set the port to either the environment variable or fallback to 8000
 
 connectDB(DB_NAME)
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`⚙️ Server is running at port: ${process.env.PORT || 8000}`);
+    app.listen(PORT, () => {
+      console.log(`⚙️ Server is running at port: ${PORT}`);
     });
   })
   .catch((err) => {
