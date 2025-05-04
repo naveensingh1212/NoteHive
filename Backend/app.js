9e6092d82f2c087c2ca799c0ef09c8a392146473
naveensingh1212 <nnaveensingh0101@gmail.com>
2025-05-04 16:22:15 +0530

    refactor: streamline environment variable loading and update CORS configuration

--- Backend/app.js
+++ Backend/app.js
@@ -3,37 +3,27 @@ import dotenv from "dotenv";
 import router from './routes/note.routes.js';
 import userRoutes from "./routes/user.routes.js";
 import cors from "cors";
+
 const app = express();
 
-dotenv.config({
-    path: './env'
-})
+// Load environment variables
+dotenv.config();
 
 const corsOptions = {
-    origin: 'http://localhost:5173/', // Frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  };
+  origin: 'https://note-hive-ten.vercel.app', // Frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 
-  app.use(cors(corsOptions));
+app.use(cors(corsOptions)); // Use CORS middleware
 
-app.use(express.json());   //middle ware to parse json data 
+app.use(express.json()); // Middleware to parse JSON requests
 
+// Routes
 app.use('/api/notes', router);
 app.use("/api/users", userRoutes);
 
+
+
 export default app;
-
-
-
-// const PORT = process.env.PORT || 8000
-
-// // app.get('/' , (req,res) =>{
-// //     res.send("hello nvn");
-// // })
-
-// // app.listen(PORT ,() => {
-// //     console.log(`this is nvn at ${PORT}`)
-// // })
-
